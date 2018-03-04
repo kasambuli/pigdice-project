b91ecc2edc1aa6e7b8cf047d0e14227ed4a7181e
Cynthia Kasambuli <cynthiakasambuli@gmail.com>
2018-03-04 23:13:03 +0300

    add player's name

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,34 @@
 function rollDice() {
   var total = 0;
-  var roll = Math.floor(Math.random() * 6) + 1;
-  if (roll != 1) {
-    total = roll + total;
+  var result = Math.floor(Math.random() * 6) + 1;
+  if (result != 1) {
+    total = total + result;
   }
   return total;
 }
+
+function Player() {
+  this.playerName = 0
+}
+var firstPlayer = new player();
 $(document).ready(function() {
-  $("#roll2").click(function(e) {
+  $("#firstRoll").click(function(e) {
     e.preventDefault();
+    console.log("firstRoll");
     $("#firstDie").text(rollDice());
+  });
+
+
+  $("#secondRoll").click(function(e) {
+    e.preventDefault();
+    console.log("secondRoll");
+    $("#secondDie").text(rollDice());
+  });
+
+  $("#playerOne").submit(function() {
+    event.preventDefault();
+    firstPlayer.playerName = $(".player2").val().toUpperCase();
+    $(".player2name").text(player2.playerName);
+    $("#player2").hide();
   });
 });
